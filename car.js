04d7ee80b049674f445fadcafd4dbe60d3679ac3
Looczky <doyouevenmv@gmail.com>
2023-05-19 14:13:31 +0200

    save

--- car.js
+++ car.js
@@ -10,14 +10,16 @@ class Car{
         this.speed = 0;
         this.acceleration = 0.2;
         this.maxSpeed = 3;
-        this.friction = 0.05
-        this.angle = 0
+        this.friction = 0.05;
+        this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update(){
         this.#move();
+        this.sensor.update();
     }
 
     #move(){
@@ -77,5 +79,7 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
