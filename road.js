fcca4d09e8ddb682bd456e78f077438aacde1de9
Looczky <doyouevenmv@gmail.com>
2023-05-21 17:18:42 +0200

    collision + road border fix

--- road.js
+++ road.js
@@ -51,6 +51,7 @@ class Road{
             ctx.beginPath();
             ctx.moveTo(border[0].x, border[0].y);
             ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
         });
     }
 }
